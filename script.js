3a0149cbed8a4d77059ddec45a40abcf0266d4e5
Andrew Essam <andrewessam-2007@hotmail.com>
2023-08-31 22:29:45 +0300

    first Update

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const inputOldPrice = document.querySelector(".inputOldPrice");
 const inputOldQuantityStocks = document.querySelector(
   ".inputOldQuantityStocks"
 );
+
+const showHideResult = document.querySelector(".resultPosition");
 const feesResult = document.querySelector(".feesResult");
 
 const inputNewPrice = document.querySelector(".inputNewPrice");
@@ -28,26 +30,38 @@ const averagePrice = () => {
 
   console.log(result);
 
-  feesResult.innerHTML = `<h1> متوسط السعر</h1> <p>${result}</p>`;
+  feesResult.innerHTML = `
+  <h1 class="showMe"> متوسط السعر</h1> <p>اجمالى عدد الاسهم: ${totalQuantity}  </p>
+   <p> متوسط السعر: ${Math.round(result)}</p>
+   <p>اجمالى المبلغ : ${newPricee} </p>
+   <button class="btnSubmit" onClick="resetView()"> مبلغ جديد</button>
+  `;
+  showHideResult.classList.remove("showHideResultPosition");
+  document.querySelector(".showMe").scrollIntoView({
+    behavior: "smooth"
+  });
 };
+function resetView() {
+  location.href = "./index.html";
+  console.log("hi");
+}
+// const feesCalculate = () => {
+//   //   console.log(Number(inputfees.value));
+//   let result =
+//     Number(inputfees.value) * 0.0005 +
+//     (Number(inputfees.value) * 0.0001 +
+//       Number(inputfees.value) * 0.0001 +
+//       1 +
+//       Number(inputfees.value) * 0.00005457);
 
-const feesCalculate = () => {
-  //   console.log(Number(inputfees.value));
-  let result =
-    Number(inputfees.value) * 0.0005 +
-    (Number(inputfees.value) * 0.0001 +
-      Number(inputfees.value) * 0.0001 +
-      1 +
-      Number(inputfees.value) * 0.00005457);
+//   feesResult.innerHTML = `<h1>
+//   اجمالى مصاريف
+//   </h1><p style="text-align:center;">${result}</p>`;
+// };
 
-  feesResult.innerHTML = `<h1>
-  اجمالى مصاريف
-  </h1><p style="text-align:center;">${result}</p>`;
-};
-
-feesButton.addEventListener("click", () => {
-  feesCalculate();
-});
+// feesButton.addEventListener("click", () => {
+//   feesCalculate();
+// });
 
 avgButton.addEventListener("click", () => {
   averagePrice();
